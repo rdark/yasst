07ed7cde9383bfbd718ff0a95667fc56421a88bc
Richard Clark <richard@fohnet.co.uk>
2016-02-11 12:46:47 +0000

    make YasstFile raise NotImplementedError

--- lib/yasst/yasst_file.rb
+++ lib/yasst/yasst_file.rb
@@ -1,4 +1,7 @@
 ##
 # Decorator pattern for File
 class YasstFile < File
+  def initialize(*_args)
+    raise NotImplementedError
+  end
 end
--- spec/unit/yasst_file_spec.rb
+++ spec/unit/yasst_file_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe YasstFile do
   let(:plain_file_name) { 'plain_file.txt' }
-  it 'should be a kind of File' do
-    expect(YasstFile.new("#{YasstTest::FIXTURES_DIR}/#{plain_file_name}"))
-      .to be_a_kind_of(File)
+  it 'should not be implmented' do
+    expect { YasstFile.new("#{YasstTest::FIXTURES_DIR}/#{plain_file_name}") }
+      .to raise_error(NotImplementedError)
   end
 end
