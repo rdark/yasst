c397fa012f054a00552e0e308172f8981af1b644
Richard Clark <richard@fohnet.co.uk>
2016-03-05 16:30:15 +0000

    add inprogress YasstFile implementation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,7 @@ require 'yasst'
 module YasstTest
   PROJECT_ROOT = File.expand_path('../../', __FILE__)
   FIXTURES_DIR = File.expand_path("#{PROJECT_ROOT}/spec/fixtures")
+  DEFAULT_ENC_FILE_EXT = 'enc'.freeze
+  Dir.mkdir("#{FIXTURES_DIR}/tmp") unless File.exist? "#{FIXTURES_DIR}/tmp"
 end
 include YasstTest
